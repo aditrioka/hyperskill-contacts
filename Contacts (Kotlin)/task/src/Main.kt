4419b8b061d7cc3ee9ee223d9dec5de8ce4336fc
Adi Trioka <aditrioka@gmail.com>
2025-01-21 09:33:08 +0700

    finish first stage

--- Contacts (Kotlin)/task/src/Main.kt
+++ Contacts (Kotlin)/task/src/Main.kt
@@ -1,5 +1,49 @@
 package contacts
 
+
+
 fun main() {
-    println("Hello World!")
+    val app = ContactsApp()
+    app.start()
+}
+
+class Contact(val name: String, val surname: String, val phoneNumber: String)
+
+class ContactsApp {
+
+    private lateinit var contact: Contact
+
+    fun start() {
+        promptInput()
+        printMessage()
+    }
+
+    private fun printMessage() {
+        println(RECORD_CREATED_MESSAGE.trimIndent())
+    }
+
+    private fun promptInput() {
+        println(NAME_PROMPT)
+        val name = readln()
+
+        println(SURNAME_PROMPT)
+        val surname = readln()
+
+        println(PHONE_PROMPT)
+        val phoneNumber = readln()
+
+        contact = Contact(name, surname, phoneNumber)
+    }
+
+    companion object {
+        const val NAME_PROMPT = "Enter the name of the person:"
+        const val SURNAME_PROMPT = "Enter the surname of the person:"
+        const val PHONE_PROMPT = "Enter the number:"
+
+        const val RECORD_CREATED_MESSAGE = """
+            
+            A record created!
+            A Phone Book with a single record created!
+        """
+    }
 }
